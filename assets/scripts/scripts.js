3c3b1a0885ccabe57f6541ca7de1d4fbebf5b826
unknown <arthur.andros.aa.aa@gmail.com>
2020-07-22 14:35:42 +0300

    join-us

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -12,3 +12,56 @@ function openBurger() {
         headerContent.style.display="flex"
     }
 };
+
+function openLevel(evt,lvlName,lvlTab) {
+    // Declare all variables
+    var i, tabcontent, tablinks, lvlIll;
+
+    //Get all elements with class="lvlIll" and hide them
+    lvlIll = document.getElementsByClassName("menu-illustration")
+    for(i=0;i<lvlIll.length;i++){
+        lvlIll[i].style.display = "none";
+    }
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("join-us-info");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("join-us-menu-choose-lvl-element");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active-btn-lvl", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    // document.getElementById(lvlName).style.display = "block";
+    document.getElementById(lvlName).style.display="block";
+    document.getElementById(lvlTab).style.display="flex";
+    evt.currentTarget.className += " active-btn-lvl";
+
+
+    //клик на мелкую хуйню
+    if(lvlTab==='beginner') {
+        document.getElementById("defaultOpenBeginner").click();
+    }else if(lvlTab==='advance'){
+        document.getElementById("defaultOpenAdvance").click();
+    }else{
+        document.getElementById("defaultOpenMiddle").click();
+    }
+}
+function openTab(evt, id){
+    let courseDescription = document.getElementsByClassName('course-description');
+    for(let i=0;i<courseDescription.length;i++){
+        courseDescription[i].style.display="none";
+    }
+    let tablinks = document.getElementsByClassName('join-us-icon-el');
+    for(let i=0;i<tablinks.length;i++){
+        tablinks[i].className = tablinks[i].className.replace("active-icon","")
+    }
+    document.getElementById(id).style.display='block';
+    evt.currentTarget.className += " active-icon";
+
+}
+document.getElementById("defaultOpen").click();
